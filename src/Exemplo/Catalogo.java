795e333361df459eead5485a0b670d7877d8c5e9
04030234 <leticia.gurjao16@hotmail.com>
2019-02-21 20:01:37 -0300

    exemplo de switch

--- src/Exemplo/Catalogo.java
+++ src/Exemplo/Catalogo.java
@@ -9,5 +9,25 @@ public class Catalogo {
             a = c;
         }
         System.out.println(a);
+        if (c>a){
+            c =a;
+        }else{
+            c = b;
+        }
+        System.out.println(c);
+        /* Switch */
+        int op = 3;
+        int v = 0;
+        switch (op){
+            case 1:
+                v =1;
+            break;
+            case 2:
+                v =2;
+            break;
+            default:
+                v = -1;
+        }
+        System.out.println(v);
     }
 }
