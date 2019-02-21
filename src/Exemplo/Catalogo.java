b02f06be3e38d400264a58ea261f8c8a4bd8dd14
04030234 <leticia.gurjao16@hotmail.com>
2019-02-21 19:48:34 -0300

    exemplos aula

---
+++ src/Exemplo/Catalogo.java
@@ -0,0 +1,13 @@
+package Exemplo;
+
+public class Catalogo {
+
+    public static void main (String[] args){
+        /*Estrutura de Seleção*/
+        int a=10, b=90, c=-100;
+        if (a>0){
+            a = c;
+        }
+        System.out.println(a);
+    }
+}
